36a382849176279afed3837c33893afa125ef25a
tenki teramura <m0118187ab@edu.teu.ac.jp>
2021-06-13 10:59:42 +0900

    カードがランダムに表示できるようにした
    同じカードが表示されないようにするのは後日

--- Assets/Script/DraftManager.cs
+++ Assets/Script/DraftManager.cs
@@ -10,22 +10,19 @@ public class DraftManager : MonoBehaviour
 
     void Start()
     {
-        /*for(int i = 0;i < 3;i++)
+        for(int i = 0;i < 3;i++)
         {
-            randomCardID[i] = Random.Range(0, 3);
-        }*/
+            randomCardID[i] = Random.Range(1, 4);
+        }
         StartGame();
     }
 
     void StartGame()
     {
-        CreateCard(1, CardSelectField);
-        CreateCard(2, CardSelectField);
-        CreateCard(3, CardSelectField);
-        /*for(int i = 0;i < 3;i++)
+        for(int i = 0;i < 3;i++)
         {
-            CreateCard(1, CardSelectField);
-        }*/
+            CreateCard(randomCardID[i], CardSelectField);
+        }
     }
 
     void CreateCard(int cardID, Transform place)
